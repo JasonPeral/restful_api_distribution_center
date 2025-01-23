ad6e66ff6060db1b2a7d226c858e22d500a305fc
Jason Peral <93687819+JasonPeral@users.noreply.github.com>
2025-01-22 21:38:25 -0500

    Delete DistributionCenterApp directory

--- DistributionCenterApp/src/main/java/com/finalProject/DistributionCenterApp/controllers/Item.java
+++
@@ -1,9 +0,0 @@
-package com.finalProject.DistributionCenterApp.controllers;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/items")
-public class Item {
-}
